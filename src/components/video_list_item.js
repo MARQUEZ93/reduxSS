60609175ac2f698d511ae52257622f86ce2ce64c
Alejandro E. Marquez <marqueza@utexas.edu>
2018-08-26 18:36:39 -0400

    pass youtube api key to youtube api search object that I get from one of my dependencies

---
+++ src/components/video_list_item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+  const video = props.video;
+  return (
+    <div>
+      <li>Video</li>
+    </div>
+  );
+}
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = "AIzaSyDY3mv3Elr4lPshOg9vXZfm3ld71YgrgAA";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props){
+
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState( { videos });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it
