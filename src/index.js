d87b11db0a9d605a0c4ad92d5e6d9354cb8e2e91
Alejandro E. Marquez <marqueza@utexas.edu>
2018-09-06 13:00:48 -0400

    changed default search

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class App extends Component {
     super(props);
 
     this.state = { videos: [], selectedVideo: null };
-    this.videoSearch('surfboards'); //default search
+    this.videoSearch('mcgregor alvarez full fight'); //default search
   }
 
   videoSearch(term) {
