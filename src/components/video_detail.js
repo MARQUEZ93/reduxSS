f37e3400d8bdea93fa094e0eb430449614b22096
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-11-23 17:03:37 -0500

    render data from api

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,20 +2,27 @@ import React from 'react';
 
 const VideoDetail = ({video}) => {
 
+  console.log(video);
   if (!video) {
     return <div>Loading...</div>;
   }
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
-
   return (
     <div className="video=detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
-        <div>{video.snippet.title}</div>
-        <div>{video.snippet.description}</div>
+        <div className="detailsTitle">{video.snippet.title}</div>
+        <div className="detailsDescription">
+          <img className="detailsImg" src={video.snippet.thumbnails.high.url} />
+          <div className="detailsDescriptionText">
+            {video.snippet.channelTitle}
+            {video.snippet.publishedAt}
+            {video.snippet.description}
+          </div>
+        </div>
       </div>
     </div>
   );
